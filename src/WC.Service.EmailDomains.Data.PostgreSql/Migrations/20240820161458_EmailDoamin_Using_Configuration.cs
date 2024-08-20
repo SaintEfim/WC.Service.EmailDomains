fc2ecdb81c0e40845e5dfba982411b7faf30e1c6
SaintEfim <saintefim2@gmail.com>
2024-08-20 19:15:13 +0300

    added migration

---
+++ src/WC.Service.EmailDomains.Data.PostgreSql/Migrations/20240820161458_EmailDoamin_Using_Configuration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WC.Service.EmailDomains.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailDoamin_Using_Configuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailDomains_DomainName",
+                table: "EmailDomains",
+                column: "DomainName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_EmailDomains_DomainName",
+                table: "EmailDomains");
+        }
+    }
+}
